300d00acac0a394fc1d4a2c15dfe01fb91f05d9f
Rodion Zherdev <r.zherdev@vigbo.com>
2025-06-01 11:21:04 +0400

    feat: Добавляет пустую функцию подсчета

--- main.go
+++ main.go
@@ -6,7 +6,26 @@ func main() {
 	usdToEuro := 0.85
 	usdToRub := 77.0
 
-	euroToRub := usdToRub / usdToEuro
+	currencyInput := currencyInput()
+	convertedValue := currencyConverter(currencyInput, usdToEuro, usdToRub)
+	fmt.Printf("%.2f", convertedValue)
+}
 
-	fmt.Print(euroToRub)
+func currencyInput() int {
+	var currency int
+
+	fmt.Println("Введите сумму в рублях: ")
+	fmt.Scanln(&currency)
+
+	if currency > 0 {
+		return currency
+	} else {
+		fmt.Println("Введите корректную сумму")
+
+		return currencyInput()
+	}
+}
+
+func currencyConverter(currencyValue int, currentCurrеncy, outputCurrency float64) float64 {
+	return 1.22
 }
